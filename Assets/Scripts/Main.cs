fd8e95288f35e358efc44cd21d3442475cd75270
smonist <ich.bin-ich@gmx.at>
2014-10-18 19:17:26 +0200

    everything done so far
    tracking the right hand

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+	public bool isMoving = false;
+	public GUIText GUIplayerInView;
+	public GUIText GUIjoint;
+	public GUIText GUIjointPosition;
+
+	// Use this for initialization
+	void Start () {
+		//QualitySettings.vSyncCount = 1;
+		GameObject Hand_Right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		uint playerID = KinectManager.Instance != null ? KinectManager.Instance.GetPlayer1ID() : 0;
+
+		if (playerID > 0) {
+			GUIplayerInView.text = "Player present";
+
+			int jointsCount = (int)KinectWrapper.NuiSkeletonPositionIndex.Count;
+			for (int i = 0; i < jointsCount; i++) {
+
+				if(KinectManager.Instance.IsJointTracked(playerID, 11)) {
+					int joint = 11;
+
+					Vector3 posJoint = KinectManager.Instance.GetJointPosition(playerID, joint);
+					GUIjointPosition.text = posJoint.ToString();
+				}
+			}
+		}
+
+		else {
+			GUIplayerInView.text = "no player";
+		}
+	}
+}
