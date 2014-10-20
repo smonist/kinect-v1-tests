be45c8dae45ca664440c7b2228aa3a0de254ef26
smonist <ich.bin-ich@gmx.at>
2014-10-20 23:09:20 +0200

    detecting acceleration or right hand

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Main : MonoBehaviour {
-	public bool isMoving = false;
+	public int frameBufferCount;
 	public GUIText GUIplayerInView;
 	public GUIText GUIjointPosition;
+	public GUIText GUIacceleration;
+	public GUIText GUIisMoving;
+
+	List<Vector3> jointCache = new List<Vector3>();
+	Vector3 acceleration;
 
 	// Use this for initialization
 	void Start () {
 		//QualitySettings.vSyncCount = 1;
+		for (int i = 0; i < frameBufferCount; i++)
+			jointCache.Add (new Vector3(0, 0, 0));
+
+		acceleration = new Vector3(0, 0, 0);
 	}
 	
 	// Update is called once per frame
@@ -27,6 +37,22 @@ public class Main : MonoBehaviour {
 
 					Vector3 posJoint = KinectManager.Instance.GetJointPosition(playerID, joint);
 					GUIjointPosition.text = posJoint.ToString();
+
+					acceleration = (posJoint - jointCache[jointCache.Count - 1]);
+
+					if (!V3Equal(acceleration,Vector3.zero)) {
+						GUIisMoving.text = "moving";
+					}
+					else {
+						GUIisMoving.text = "-";
+					}
+
+					GUIacceleration.text = acceleration.ToString();
+
+					//prepare jointcache for next frame
+					jointCache.Insert(0, posJoint);
+					jointCache.RemoveAt(jointCache.Count - 1);
+					
 				}
 			}
 		}
@@ -34,5 +60,9 @@ public class Main : MonoBehaviour {
 		else {
 			GUIplayerInView.text = "no player";
 		}
+	}
+
+	public bool V3Equal(Vector3 a, Vector3 b){
+		return Vector3.SqrMagnitude(a - b) < 0.0001;
 	}
 }
